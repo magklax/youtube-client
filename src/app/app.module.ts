3e2cb4629863b5f7343397984dac5f16c7eb4266
Nadezda <nadezda.prokopyeva@brytlyt.com>
2021-08-02 23:02:59 +0200

    feat: add components, eslint config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SearchItemComponent } from './components/search/search-item/search-item.component';
+import { SearchResultsComponent } from './components/search/search-results/search-results.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent, SearchItemComponent, SearchResultsComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
